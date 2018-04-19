7a91dc682b881c126e349fd103f8dd938b0fa117
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2018-04-19 11:43:30 +0700

    Init project base code form teacher

---
+++ consts/firebase_info.js
@@ -0,0 +1,31 @@
+export const config = {
+  apiKey: "AIzaSyBvmHq9pKyP8ZpiwDLMFyVgEg2U0j_BGEE",
+    authDomain: "reactnativelogin-de4e2.firebaseapp.com",
+    databaseURL: "https://reactnativelogin-de4e2.firebaseio.com",
+    projectId: "reactnativelogin-de4e2",
+    storageBucket: "reactnativelogin-de4e2.appspot.com",
+    messagingSenderId: "222315865786"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const config = {
+//   apiKey: "AIzaSyDaY9hbGQOr41ljQeZgX26NTd_SqKwH4oQ",
+//   authDomain: "studentloan-7fbce.firebaseapp.com",
+//   databaseURL: "https://studentloan-7fbce.firebaseio.com",
+//   projectId: "studentloan-7fbce",
+//   storageBucket: "studentloan-7fbce.appspot.com",
+//   messagingSenderId: "1016135551882"
+// };
