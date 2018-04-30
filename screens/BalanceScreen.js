beef89af74a30974dbedf6be5c363af9301634b3
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2018-04-30 16:06:57 +0700

    Show data from database

--- screens/BalanceScreen.js
+++ screens/BalanceScreen.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
-import { Text} from 'react-native';
+import { Text, View, FlatList} from 'react-native';
+import { List, ListItem} from "react-native-elements"
+import * as firebase from 'firebase';
 
 class BalanceScreen extends Component {
+
+    static navigationOptions = {
+        title: "My Database",
+        header: null
+    }
+    
+    constructor(props){
+        super(props);
+    
+        this.state = {
+          data: [],
+        };
+
+        this.itemsRef = this.getRef().child('myimgs');
+    }
+
+    getRef(){
+        return firebase.database().ref();
+    }
+    
+    componentDidMount(){
+        this.getItems(this.itemsRef);
+    }
+
+    getItems(itemsRef){
+    itemsRef.on('value', (data) => {
+            let items = [];
+            data.forEach((child) => {
+                items.push({
+                    title : child.val().title,
+                    img : child.val().img,
+                    _key: child.key
+                });
+            });
+
+            console.log(items);
+            this.setState({
+                data: items
+            });
+        })
+    }
+      
+    
+     
+
     render() {
         return (
-          <Text>Another Screen</Text>  
+            <List containerStyle={{borderTopWidth: 0, borderBottomWidth: 0}}>
+            <FlatList
+              data = {this.state.data}
+              renderItem={
+                ({item}) => (
+                  <ListItem
+                    roundAvatar
+                    title={`${item.title}`}
+                    avatar={{uri: item.img}}
+                    containerStyle={{borderBottomWidth:0}}
+                  />
+                )}
+                keyExtractor={item => item._key}
+            
+            />
+            </List>
         );
     }
 }
